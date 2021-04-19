11764e34a12fe131e5445bd984b381581da8b940
wiktorpp <grazynapp65@gmail.com>
2021-04-19 20:10:09 +0200

    First commit

---
+++ mandelbrot.py
@@ -0,0 +1,36 @@
+MAX_ITER = 80
+
+def mandelbrot(c):
+    z = 0
+    n = 0
+    while abs(z) <= 2 and n < MAX_ITER:
+        z = z*z + c
+        n += 1
+    return n
+
+out1 = []
+if __name__ == "__main__":
+    for imaginary in range(-200, 200):
+        out2 = []
+        for real in range(-300, 100):
+            out2.append(mandelbrot(complex(real / 200, imaginary / 200)) == MAX_ITER)
+        out1.append(out2)
+
+    #for i in out1:
+    #    for j in i:
+    #        if j == True:
+    #            print("#", end="")
+    #        else:
+    #            print(" ", end="")
+    #    print()
+
+from semigraphics import braille
+for i in range(0, len(out1), 5):
+    for j in range(0, len(out1[0]), 2):
+        try:
+            x = tuple([tuple(k[j:j+2]) for k in tuple(out1[i:i+3])])
+            #print(tuple(x))
+            print(braille[x], end='')
+        except:
+            print("#", end='')
+    print()
