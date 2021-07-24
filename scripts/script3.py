d82a7f0e7c35591619371530ebb52b3fae5f3df8
wiktorpp <grazynapp65@gmail.com>
2021-07-24 19:10:14 +0200

    Added a script to scripts folder

--- scripts/script3.py
+++ scripts/script3.py
@@ -1,8 +1,8 @@
 out={}
-for i in range(10241, 10496): 
-    print(chr(i))
+for i, char in enumerate(range(10240, 10496)): 
+    print(i)
     print("⣿⣿⣿")
-    print(f"⣿{chr(i)}⣿")
+    print(f"⣿{chr(char)}⣿")
     print("⣿⣿⣿")
     inp=list(reversed(("000000000000000" + bin(i)[2:])[-8:]))
     out[(
@@ -10,5 +10,5 @@ for i in range(10241, 10496):
         (inp[1] == "1", inp[4] == "1"),
         (inp[2] == "1", inp[5] == "1"),
         (inp[6] == "1", inp[7] == "1"),
-    )]=chr(i)
+    )]=chr(char)
     print(out)
