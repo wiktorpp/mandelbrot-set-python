b260b89655aa4e42b34f9709d735cd5c215966f3
wiktorpp <grazynapp65@gmail.com>
2021-07-24 19:14:42 +0200

    Fixed bug

--- mandelbrot.py
+++ mandelbrot.py
@@ -16,12 +16,12 @@ if __name__ == "__main__":
             out2.append(mandelbrot(complex(real / 200, imaginary / 200)) == MAX_ITER)
         out1.append(out2)
 
-from semigraphics import braille
-for i in range(0, len(out1), 5):
-    for j in range(0, len(out1[0]), 2):
-        try:
-            x = tuple([tuple(k[j:j+2]) for k in tuple(out1[i:i+4])])
-            print(braille[x], end='')
-        except:
-            print("#", end='')
-    print()
+    from semigraphics import braille
+    for i in range(0, len(out1), 5):
+        for j in range(0, len(out1[0]), 2):
+            try:
+                x = tuple([tuple(k[j:j+2]) for k in tuple(out1[i:i+4])])
+                print(braille[x], end='')
+            except:
+                print("#", end='')
+        print()
