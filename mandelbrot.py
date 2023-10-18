c09e083729d94616a92b58a4bf17585585347689
Wiktor Pyk <56189344+wiktorpp@users.noreply.github.com>
2023-10-18 13:28:56 +0200

    Cleanup. Removed braille module

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,27 +1,35 @@
-MAX_ITER = 80
+max_iter = 80
+scale = 200
 
 def mandelbrot(c):
     z = 0
     n = 0
-    while abs(z) <= 2 and n < MAX_ITER:
-        z = z*z + c
+    while abs(z) <= 2 and n < max_iter:
+        z = z * z + c
         n += 1
     return n
 
-out1 = []
+out = []
 if __name__ == "__main__":
-    for imaginary in range(-200, 200):
-        out2 = []
-        for real in range(-300, 100):
-            out2.append(mandelbrot(complex(real / 200, imaginary / 200)) == MAX_ITER)
-        out1.append(out2)
+    for imaginary in range(-scale, scale):
+        line = []
+        for real in range(-int(scale * 1.5), int(scale * 0.5)):
+            line.append(mandelbrot(complex(real / 200, imaginary / 200)) == max_iter)
+        out.append(line)
 
-    from semigraphics import braille
-    for i in range(0, len(out1), 5):
-        for j in range(0, len(out1[0]), 2):
+    for j in range(0, len(out), 4):
+        for i in range(0, len(out[0]), 2):
             try:
-                x = tuple([tuple(k[j:j+2]) for k in tuple(out1[i:i+4])])
-                print(braille[x], end='')
-            except:
-                print("#", end='')
+                char = bytearray([0b11100010, 0b10100000, 0b10000000])
+                char[2] = (char[2] & ~0b1)      | 0b1      if out[j][i]         else char[2]
+                char[2] = (char[2] & ~0b1000)   | 0b1000   if out[j][i + 1]     else char[2]
+                char[2] = (char[2] & ~0b10)     | 0b10     if out[j + 1][i]     else char[2]
+                char[2] = (char[2] & ~0b10000)  | 0b10000  if out[j + 1][i + 1] else char[2]
+                char[2] = (char[2] & ~0b100)    | 0b100    if out[j + 2][i]     else char[2]
+                char[2] = (char[2] & ~0b100000) | 0b100000 if out[j + 2][i + 1] else char[2]
+                char[1] = (char[1] & ~0b1)      | 0b1      if out[j + 3][i]     else char[1]
+                char[1] = (char[1] & ~0b10)     | 0b10     if out[j + 3][i + 1] else char[1]
+                print(char.decode("utf-8"), end="")
+            except IndexError:
+                print("#", end="")
         print()
--- scripts/script2.py
+++
@@ -1,7 +0,0 @@
-from braille import charDict
-for key, value in charDict.items():
-    print(
-        f"({tuple(value[0])},\n"
-        f" {tuple(value[1])},\n"
-        f" {tuple(value[2])}): \"{key}\","
-    )
